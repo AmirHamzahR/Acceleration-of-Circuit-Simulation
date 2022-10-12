988ab8b4b6f26cc8ea60d06588880c24eaa2821b
AmirHamzahR <s1945267@ed.ac.uk>
2022-10-12 20:54:34 +0100

    Added voltage sources in the code

--- circuit_test/Python/simple_linear.py
+++ circuit_test/Python/simple_linear.py
@@ -5,19 +5,37 @@ Expansions:
 1) Try to use LU decomposition to solve the circuit
 2) Increase the size of the circuit
 """
-
+from importlib.abc import ResourceReader
+import time as tm
 import numpy as np
 
-# A function that defines the different stamps for the resistor, voltage source, and current source
+#Functions that defines the different stamps for the resistor, voltage source, and current source
 def cond(R):
     g = 1/R
     return g
 
+# A function that creates a matrix of zeros
 def matrix(maxi,maxj):
     size = (maxi,maxj)
     return np.zeros(size)
 
-def Is_assigner(x,node1,node2,maxi,maxj):
+# A function that extends the branch of the matrix
+def mat_ext(M):
+    a = np.shape(M)
+    M1=np.zeros((a[0]+1,a[1]+1),M.dtype)
+    M1[:-1,:-1]=M
+    return M1
+
+# Function that sums up the matrices for the total overall matrix
+def mat_sum(list_of_mat):
+    a = list_of_mat[0]
+    for i in range(1, len(list_of_mat)):
+        a += list_of_mat[i]
+    return a
+
+# A function that initiliaze the current sources value and input into the RHS matrices based on MNA stamps
+def Is_assigner(node1,node2,x,maxi,maxj):
+    maxj = 1
     a = matrix(maxi,maxj)
     if(node1 == 0):
         a[node2-1][0] = x
@@ -28,9 +46,8 @@ def Is_assigner(x,node1,node2,maxi,maxj):
         a[node2-1][maxj-1] = -x
     return a
 
-
-
-def R_assigner(x,node1,node2,maxi,maxj):
+# A function that initiliaze the resistor values and input into the LHS matrices based on MNA stamps
+def R_assigner(node1,node2,x,maxi,maxj):
     a = matrix(maxi,maxj)
     if(node1 == 0):
         a[node2-1,node2-1] = x
@@ -42,6 +59,37 @@ def R_assigner(x,node1,node2,maxi,maxj):
         a[node2-1,node1-1] = -x
         a[node2-1,node2-1] = x
     return a
+
+# A function that adds in voltage for the RHS based on MNA stamps
+def Vs_assigner(M,x):
+    Value = np.array([[x]])
+    a = np.concatenate((M, Value), axis=0)
+    return a
+
+# A function that extends the matrix with the values needed for the LHS based on MNA stamps
+def branch_ext(M,node1,node2):
+    # Create the column for voltage stamp
+    size_a = np.shape(M)
+    va= np.zeros((size_a[0],1))
+    if(node1 == 0):
+        va[node2-1,0] = 1
+    elif(node2 == 0):
+        va[node1-1,0] = -1
+    else:
+        va[node1-1,0] = 1
+        va[node2-1,0] = -1
+    
+    # Create the row for voltage stamp
+    zero_ext = np.zeros((1,1))
+    ha = va[..., None]
+    haz = np.concatenate((ha,zero_ext),axis=None)
+    
+    #Contatenate both for the new matrix
+    M1 = np.hstack((M,va))
+    M2 = np.vstack((M1,haz))
+    
+    return M2
+    
 
 def plu(A):
     
@@ -78,7 +126,7 @@ def forward_substitution(L, b):
     n = L.shape[0]
     
     #Creating the solution vector
-    y = np.zeros_like(b, dtype=np.double);
+    y = np.zeros_like(b, dtype=np.double)
     
     #Here we perform the forward-substitution.  
     #Initializing  with the first row.
@@ -98,7 +146,7 @@ def back_substitution(U, y):
     n = U.shape[0]
     
     #Creating the solution vector
-    x = np.zeros_like(y, dtype=np.double);
+    x = np.zeros_like(y, dtype=np.double)
 
     #Here we perform the back-substitution.  
     #Initializing with the last row.
@@ -121,42 +169,72 @@ def plu_solve(A, b):
     return back_substitution(U, y)
 
 #Values of the variables
-Va = 0
-Vb = 0
 
-I1 = 1
-I2 = 2
-I3 = 3
+Vs = [
+    3
+]
 
-R1 = 1000
-R2 = 2000
-R3 = 3000
-R4 = 4000
-R5 = 5000
-R6 = 6000
+I = [
+    3
+]
 
-Ia = Is_assigner(I1,1,3,5,1)
-Ib = Is_assigner(I2,2,0,5,1)
-Ic = Is_assigner(I3,5,4,5,1)
-#Need to make this more generic
-RHS = Ia + Ib + Ic
+R = [
+    2,3,4,2,1
+]
+
+# total number of nodes and voltage sources that is contained to build the base matrix 
+# that contains only zeros which will then be occupied with values from the components
+T_nodes = 4
+
+# Size of matrix
+Maxi = T_nodes
+Maxj = Maxi
+
+
+# Resistor values in a similar format of SPICE simulators's netlist
+I_stamp = [
+    Is_assigner(0,4,I[0],Maxi,Maxj),
+]
+# Adding the current values from the stamp to create the overall RHS matrix 
+T_I = mat_sum(I_stamp)
+
+#Adding the voltage source values
+RHS = Vs_assigner(T_I,Vs[0])
+
 print(RHS)
 
-a = R_assigner(cond(R1),1,2,5,5)
-b = R_assigner(cond(R2),1,0,5,5)
-c = R_assigner(cond(R3),3,0,5,5)
-d = R_assigner(cond(R4),2,4,5,5)
-e = R_assigner(cond(R5),4,0,5,5)
-f = R_assigner(cond(R6),3,5,5,5)
+R_stamp = [
+    R_assigner(1,2,cond(R[0]),Maxi,Maxj),
+    R_assigner(2,0,cond(R[1]),Maxi,Maxj),
+    R_assigner(2,3,cond(R[2]),Maxi,Maxj),
+    R_assigner(3,0,cond(R[3]),Maxi,Maxj),
+    R_assigner(3,4,cond(R[4]),Maxi,Maxj)
+]
 
-#Need to make this more generic
-LHS = a + b + c + d + e + f
+# Adding the resistor values from the stamp to create the overall RHS matrix
+T_R = mat_sum(R_stamp)
+
+#Adding the branch values from different stamp sources (eg. Voltage source, VCCS)
+LHS = branch_ext(T_R,0,1)
 print(LHS)
 
 # A function that could analyse and use the stamps to create an MNA circuit matrix
-
+# get the start time
+#st1 = time.time()
 # Solving the MNA matrix by using linear solvers in python
-ans1 = np.linalg.solve(LHS,RHS)
+#ans1 = np.linalg.solve(LHS,RHS)
+# get the end time
+#et1 = time.time()
+
+st2 = tm.time()
 ans2 = plu_solve(LHS,RHS)
-print(ans1)
+# get the end time
+et2 = tm.time()
+
+#print(ans1)
 print(ans2)
+
+# get the execution time
+# elapsed_time1 = et1 - st1
+elapsed_time2 = et2 - st2
+print('Execution time:', elapsed_time2, 'seconds')
---
+++ circuit_test/Python/tests.py
@@ -0,0 +1,46 @@
+'''Just an area to test out different functions if it works or not'''
+import numpy as np
+
+def mat_ext(M):
+    a = np.shape(M)
+    M1=np.zeros((a[0]+1,a[1]+1),M.dtype)
+    M1[:-1,:-1]=M
+    return M1
+
+def Vs_assigner(M,x):
+    Value = np.array([[x]])
+    a = np.concatenate((M, Value), axis=0)
+    return a
+
+def add_values(M,node1,node2):
+    # Create the column for voltage stamp
+    size_a = np.shape(M)
+    va= np.zeros((size_a[0],1))
+    va[node1-1,0] = 1
+    va[node2-1,0] = -1
+    print(va)
+    
+    # Create the row for voltage stamp
+    zero_ext = np.zeros((1,1))
+    ha = va[..., None]
+    haz = np.concatenate((ha,zero_ext),axis=None)
+    
+    #Contatenate both for the new matrix
+    M1 = np.hstack((M,va))
+    M2 = np.vstack((M1,haz))
+    
+    return M2
+
+M=np.arange(16).reshape((4,4))    
+
+M1=np.arange(4).reshape((4,1))    
+
+print(Vs_assigner(M1,20))
+
+print(M)
+#a = mat_ext(M)
+a = add_values(M,2,3)
+b = add_values(a,1,2)
+
+print(a)
+print(b)
