0be04b002be087b864d7c06b46b5fe3d43035ec1
AmirHamzahR <s1945267@ed.ac.uk>
2022-10-22 14:13:20 +0100

    Added the section on analysing the non-linear part of the diode

--- circuit_test/Python/simple_linear.py
+++ circuit_test/Python/simple_linear.py
@@ -8,6 +8,7 @@ Expansions:
 
 import time as tm
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Function for the iterations of a set of unknowns that we are trying to solve
 def X(i):
@@ -69,9 +70,26 @@ def R_assigner(node1,node2,x,maxi,maxj):
         a[node2-1,node2-1] = x
     return a
 
+def Diode_assigner(node1,node2,Is,VT,maxi,maxj):
+    v1 = 0
+    v2 = 0
+    x = Is*(np.exp((v1-v2)/VT)-1)
+    a = matrix(maxi,maxj)
+    if(node1 == 0):
+        a[node2-1,node2-1] = x
+    elif(node2 == 0):
+        a[node1-1,node1-1] = x
+    else:
+        a[node1-1,node1-1] = x
+        a[node1-1,node2-1] = -x
+        a[node2-1,node1-1] = -x
+        a[node2-1,node2-1] = x
+    return a,v1,v2
+
 # A function that adds in voltage for the RHS based on MNA stamps
 def Vs_assigner(M,x):
     Value = np.array([[x]])
+    
     a = np.concatenate((M, Value), axis=0)
     return a
 
@@ -169,6 +187,81 @@ def back_substitution(U, y):
         
     return x
 
+def Diode_system(LHS, RHS, node1, node2, Is, VT, maxi,maxj):
+    """
+    Solve nonlinear system F=0 by Newton's method.
+    J is the Jacobian of F. Both F and J must be functions of x.
+    At input, x holds the start value. The iteration continues
+    until ||F|| < eps.
+    """
+    eps = 1e-9
+    error = 9e9
+    iteration_counter = 0
+    v = np.ones((maxi+1,1))
+    
+    x = (Is/VT)*(np.exp((v[node1-1]-v[node2-1])/VT))
+    
+    x1 = x*(v[node1-1]-v[node2-1])-Is*(np.exp((v[node1-1]-v[node2-1])/VT)-1)
+    
+    b = matrix(maxi+1,1)
+    if(node1 == 0):
+        b[node2-1][0] = x1
+    elif(node2 == 0):
+        b[node1-1][0] = -x1
+    else:
+        b[node1-1][0] = x1
+        b[node2-1][0] = -x1
+    F_x = RHS + b
+
+    a = matrix(maxi+1,maxj+1)
+    if(node1 == 0):
+        a[node2-1,node2-1] = x
+    elif(node2 == 0):
+        a[node1-1,node1-1] = x
+    else:
+        a[node1-1,node1-1] = x
+        a[node1-1,node2-1] = -x
+        a[node2-1,node1-1] = -x
+        a[node2-1,node2-1] = x
+    J_x = LHS + a
+    
+    while error > eps and iteration_counter < 5000:
+        
+        delta = np.linalg.solve(J_x, np.matmul(J_x,v) - F_x)
+        error = np.max(np.abs(delta))
+        v = v - delta
+        
+        x = (Is/VT)*(np.exp((v[node1-1]-v[node2-1])/VT))
+        
+        x1 = x*(v[node1-1]-v[node2-1])-Is*(np.exp((v[node1-1]-v[node2-1])/VT)-1)
+        if(node1 == 0):
+            b[node2-1][0] = x1
+        elif(node2 == 0):
+            b[node1-1][0] = -x1
+        else:
+            b[node1-1][0] = x1
+            b[node2-1][0] = -x1
+        F_x = RHS + b
+        
+        if(node1 == 0):
+            a[node2-1,node2-1] = x
+        elif(node2 == 0):
+            a[node1-1,node1-1] = x
+        else:
+            a[node1-1,node1-1] = x
+            a[node1-1,node2-1] = -x
+            a[node2-1,node1-1] = -x
+            a[node2-1,node2-1] = x
+        J_x = LHS + a
+        iteration_counter += 1
+        
+
+    # Here, either a solution is found, or too many iterations
+    if error > eps:
+        iteration_counter = -1
+    return v
+
+
 def plu_solve(A, b):
     
     P, L, U = plu(A)
@@ -178,22 +271,23 @@ def plu_solve(A, b):
     return back_substitution(U, y)
 
 #Values of the variables
-
+Is = 3e-9 # A
+VT = 0.05 # V
 Vs = [
-    3
+    5
 ]
 
 I = [
-    3
+    0
 ]
 
 R = [
-    2,3,4,2,1
+    1e3,4e3,3e3,2e3
 ]
 
 # total number of nodes and voltage sources that is contained to build the base matrix 
 # that contains only zeros which will then be occupied with values from the components
-T_nodes = 4
+T_nodes = 3
 
 # Size of matrix
 Maxi = T_nodes
@@ -202,33 +296,32 @@ Maxj = Maxi
 
 # Resistor values in a similar format of SPICE simulators's netlist
 I_stamp = [
-    Is_assigner(0,4,I[0],Maxi,Maxj),
+    Is_assigner(0,0,I[0],Maxi,Maxj)
 ]
 # Adding the current values from the stamp to create the overall RHS matrix 
-T_I = mat_sum(I_stamp)
+T_RHS = mat_sum(I_stamp)
 
 #Adding the voltage source values
-RHS = Vs_assigner(T_I,Vs[0])
+RHS = Vs_assigner(T_RHS,Vs[0])
 
 print(RHS)
 
 R_stamp = [
-    R_assigner(1,2,cond(R[0]),Maxi,Maxj),
-    R_assigner(2,0,cond(R[1]),Maxi,Maxj),
+    R_assigner(1,3,cond(R[0]),Maxi,Maxj),
+    R_assigner(1,0,cond(R[1]),Maxi,Maxj),
     R_assigner(2,3,cond(R[2]),Maxi,Maxj),
-    R_assigner(3,0,cond(R[3]),Maxi,Maxj),
-    R_assigner(3,4,cond(R[4]),Maxi,Maxj)
+    R_assigner(2,0,cond(R[3]),Maxi,Maxj)
 ]
 
 # Adding the resistor values from the stamp to create the overall RHS matrix
 T_R = mat_sum(R_stamp)
 
 # Adding the branch values from different stamp sources (eg. Voltage source, VCCS)
-LHS = branch_ext(T_R,0,1)
+LHS = branch_ext(T_R,0,3)
 print(LHS)
 
 # Create the function matrices of the variables that are going to be used
-new_LHS = np.dot(LHS, X(LHS))
+# new_LHS = np.dot(LHS, X(LHS))
 
 
 # A function that could analyse and use the stamps to create an MNA circuit matrix
@@ -240,14 +333,27 @@ new_LHS = np.dot(LHS, X(LHS))
 #et1 = time.time()
 
 st2 = tm.time()
-ans2 = plu_solve(LHS,RHS)
+ans = Diode_system(LHS, RHS, 1, 2, Is, VT, Maxi, Maxj)
 # get the end time
 et2 = tm.time()
 
 #print(ans1)
-print(ans2)
+print(ans)
+print(ans[1]-ans[0])
 
 # get the execution time
 # elapsed_time1 = et1 - st1
 elapsed_time2 = et2 - st2
 print('Execution time:', elapsed_time2, 'seconds')
+
+# plt.plot(t, x[0], color = 'red', label = '$x_1$')
+# plt.plot(t, x[1], color = 'blue', label = '$x_2$')
+# plt.xlabel('Time ($s$)')
+# plt.ylabel('Variables ($x_1$, $x_2$)')
+# plt.title('Variation of the Variables ($x_1$, $x_2$) with Time\n')
+# plt.legend()
+# plt.grid(which='major')
+# plt.minorticks_on()
+# plt.grid(which='minor', alpha=0.2)
+
+# plt.show()
