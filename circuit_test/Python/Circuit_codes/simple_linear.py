aa6a8328a3ea7a12fd7cfc5529ced5cac70eb567
AmirHamzahR <s1945267@ed.ac.uk>
2022-10-31 22:47:32 +0000

    Transient update for resistive network

--- circuit_test/Python/Circuit_codes/simple_linear.py
+++ circuit_test/Python/Circuit_codes/simple_linear.py
@@ -15,9 +15,43 @@ def X(i):
     x = np.zeros((len(i),1))
     return x
 
-# Function to add the matrices within each other after each iteration?
+# Doing the sine wave analysis
+def V_sine(V_o,V_a,t_d,theta,f,t1):
+    V_t1 = 0
+    if(t1 < t_d):
+        V_t1 = V_o
+    else:
+        V_t1 = V_o + V_a*np.exp(-theta*(t1-t_d))*np.sin(2*np.pi*f*(t1-t_d))
+    return V_t1
 
+# Doing the pulse wave analysis
+def V_pulse(V1,V2,t1,td,tr,tf,tpw,tper):
+    V_t1 = 0
+    if(t1 >= 0 and t1 < td):
+        V_t1 = V1
+    elif(t1 >= td and t1 < (td + tr)):
+        V_t1 = V1 + (V2-V1)*(t1-td)/tr
+    elif(t1 >= (td + tr) and t1 < (td + tr + tpw)):
+        V_t1 = V2
+    elif(t1 >= (td + tr + tpw) and t1 < (td + tr + tpw + tf)):
+        V_t1 = V2 + (V1 - V2)*(t1-(td+tr+tpw))/tf
+    elif(t1 >= (td + tr + tpw + tf) and t1 <= (td + tper)):
+        V_t1 = V1
+    else:
+        t1 = td
+    return V_t1, t1
 
+# The amount of iterations for the timestep, the higher the more accurate but uses more computing resources
+n = 5001
+
+# Defining the Time and Timestep for Transient Simulation
+X1 = np.ones((n,1))
+t_start = 0
+t_end = 12e-3
+t1 = 0
+h = (t_end - t_start) / (n-1)
+t = np.arange(t_start, t_end + h, h)
+simul = 4
 
 # Functions that defines the different stamps for the resistor, voltage source, and current source
 def cond(R):
@@ -44,69 +78,70 @@ def mat_sum(list_of_mat):
     return a
 
 # A function that initiliaze the current sources value and input into the RHS matrices based on MNA stamps
-def Is_assigner(node1,node2,x,maxi,maxj):
+def Is_assigner(node_x,node_y,x,maxi,maxj):
     maxj = 1
     a = matrix(maxi,maxj)
-    if(node1 == 0):
-        a[node2-1][0] = x
-    elif(node2 == 0):
-        a[node1-1][0] = -x
+    if(node_x == 0):
+        a[node_y-1][0] = x
+    elif(node_y == 0):
+        a[node_x-1][0] = -x
     else:
-        a[node1-1][maxj-1] = x
-        a[node2-1][maxj-1] = -x
+        a[node_x-1][maxj-1] = x
+        a[node_y-1][maxj-1] = -x
     return a
 
 # A function that initiliaze the resistor values and input into the LHS matrices based on MNA stamps
-def R_assigner(node1,node2,x,maxi,maxj):
+def R_assigner(node_x,node_y,x,maxi,maxj):
     a = matrix(maxi,maxj)
-    if(node1 == 0):
-        a[node2-1,node2-1] = x
-    elif(node2 == 0):
-        a[node1-1,node1-1] = x
+    if(node_x == 0):
+        a[node_y-1,node_y-1] = x
+    elif(node_y == 0):
+        a[node_x-1,node_x-1] = x
     else:
-        a[node1-1,node1-1] = x
-        a[node1-1,node2-1] = -x
-        a[node2-1,node1-1] = -x
-        a[node2-1,node2-1] = x
+        a[node_x-1,node_x-1] = x
+        a[node_x-1,node_y-1] = -x
+        a[node_y-1,node_x-1] = -x
+        a[node_y-1,node_y-1] = x
     return a
 
-def Diode_assigner(node1,node2,Is,VT,maxi,maxj):
+def Diode_assigner(node_x,node_y,Is,VT,maxi,maxj):
     v1 = 0
     v2 = 0
     x = Is*(np.exp((v1-v2)/VT)-1)
     a = matrix(maxi,maxj)
-    if(node1 == 0):
-        a[node2-1,node2-1] = x
-    elif(node2 == 0):
-        a[node1-1,node1-1] = x
+    if(node_x == 0):
+        a[node_y-1,node_y-1] = x
+    elif(node_y == 0):
+        a[node_x-1,node_x-1] = x
     else:
-        a[node1-1,node1-1] = x
-        a[node1-1,node2-1] = -x
-        a[node2-1,node1-1] = -x
-        a[node2-1,node2-1] = x
+        a[node_x-1,node_x-1] = x
+        a[node_x-1,node_y-1] = -x
+        a[node_y-1,node_x-1] = -x
+        a[node_y-1,node_y-1] = x
     return a,v1,v2
 
 # A function that adds in voltage for the both LHS and RHS based on MNA stamps
-def Vs_assigner(M,x,G_mat, node2, node1):
-    Value = np.array([[x]])
+def Vs_assigner(RHS,V_value,G_mat, node_y, node_x):
+    Value = np.array([[V_value]])
     # Extending the branch at the LHS matrix
-    G_mat1 = branch_ext(G_mat,node2,node1)
+    New_LHS = branch_ext(G_mat,node_y,node_x)
     # Assigning the value at RHS
-    a = np.concatenate((M, Value), axis=0)
-    return a, G_mat1
+    New_RHS = np.concatenate((RHS, Value), axis=0)
+    size_RHS = np.shape(New_RHS)
+    return New_RHS, New_LHS, size_RHS[0]
 
 # A function that extends the matrix with the values needed for the LHS based on MNA stamps
-def branch_ext(M,node1,node2):
+def branch_ext(M,node_x,node_y):
     # Create the column for voltage stamp
     size_a = np.shape(M)
     va= np.zeros((size_a[0],1))
-    if(node1 == 0):
-        va[node2-1,0] = 1
-    elif(node2 == 0):
-        va[node1-1,0] = -1
+    if(node_x == 0):
+        va[node_y-1,0] = 1
+    elif(node_y == 0):
+        va[node_x-1,0] = -1
     else:
-        va[node1-1,0] = 1
-        va[node2-1,0] = -1
+        va[node_x-1,0] = 1
+        va[node_y-1,0] = -1
     
     # Create the row for voltage stamp
     zero_ext = np.zeros((1,1))
@@ -189,7 +224,7 @@ def back_substitution(U, y):
         
     return x
 
-def Diode_system(LHS, RHS, node1, node2, Is, VT, maxi,maxj):
+def Diode_system(LHS, RHS, node_x, node_y, Is, VT, maxi,maxj):
     """
     Solve nonlinear system F=0 by Newton's method.
     J is the Jacobian of F. Both F and J must be functions of x.
@@ -201,59 +236,59 @@ def Diode_system(LHS, RHS, node1, node2, Is, VT, maxi,maxj):
     iteration_counter = 0
     v = np.ones((maxi+1,1))
     
-    x = (Is/VT)*(np.exp((v[node1-1]-v[node2-1])/VT))
+    x = (Is/VT)*(np.exp((v[node_x-1]-v[node_y-1])/VT))
     
-    x1 = x*(v[node1-1]-v[node2-1])-Is*(np.exp((v[node1-1]-v[node2-1])/VT)-1)
+    x1 = x*(v[node_x-1]-v[node_y-1])-Is*(np.exp((v[node_x-1]-v[node_y-1])/VT)-1)
     
     b = matrix(maxi+1,1)
-    if(node1 == 0):
-        b[node2-1][0] = x1
-    elif(node2 == 0):
-        b[node1-1][0] = -x1
+    if(node_x == 0):
+        b[node_y-1][0] = x1
+    elif(node_y == 0):
+        b[node_x-1][0] = -x1
     else:
-        b[node1-1][0] = x1
-        b[node2-1][0] = -x1
-    F_x = RHS + b
+        b[node_x-1][0] = x1
+        b[node_y-1][0] = -x1
+    G_x = RHS + b
 
     a = matrix(maxi+1,maxj+1)
-    if(node1 == 0):
-        a[node2-1,node2-1] = x
-    elif(node2 == 0):
-        a[node1-1,node1-1] = x
+    if(node_x == 0):
+        a[node_y-1,node_y-1] = x
+    elif(node_y == 0):
+        a[node_x-1,node_x-1] = x
     else:
-        a[node1-1,node1-1] = x
-        a[node1-1,node2-1] = -x
-        a[node2-1,node1-1] = -x
-        a[node2-1,node2-1] = x
+        a[node_x-1,node_x-1] = x
+        a[node_x-1,node_y-1] = -x
+        a[node_y-1,node_x-1] = -x
+        a[node_y-1,node_y-1] = x
     J_x = LHS + a
     
     while error > eps and iteration_counter < 5000:
         
-        delta = np.linalg.solve(J_x, np.matmul(J_x,v) - F_x)
+        delta = np.linalg.solve(J_x, np.matmul(J_x,v) - G_x)
         error = np.max(np.abs(delta))
-        v = v- delta
+        v = v - delta
         
-        x = (Is/VT)*(np.exp((v[node1-1]-v[node2-1])/VT))
+        x = (Is/VT)*(np.exp((v[node_x-1]-v[node_y-1])/VT))
         
-        x1 = x*(v[node1-1]-v[node2-1])-Is*(np.exp((v[node1-1]-v[node2-1])/VT)-1)
-        if(node1 == 0):
-            b[node2-1][0] = x1
-        elif(node2 == 0):
-            b[node1-1][0] = -x1
+        x1 = x*(v[node_x-1]-v[node_y-1])-Is*(np.exp((v[node_x-1]-v[node_y-1])/VT)-1)
+        if(node_x == 0):
+            b[node_y-1][0] = x1
+        elif(node_y == 0):
+            b[node_x-1][0] = -x1
         else:
-            b[node1-1][0] = x1
-            b[node2-1][0] = -x1
-        F_x = RHS + b
+            b[node_x-1][0] = x1
+            b[node_y-1][0] = -x1
+        G_x = RHS + b
         
-        if(node1 == 0):
-            a[node2-1,node2-1] = x
-        elif(node2 == 0):
-            a[node1-1,node1-1] = x
+        if(node_x == 0):
+            a[node_y-1,node_y-1] = x
+        elif(node_y == 0):
+            a[node_x-1,node_x-1] = x
         else:
-            a[node1-1,node1-1] = x
-            a[node1-1,node2-1] = -x
-            a[node2-1,node1-1] = -x
-            a[node2-1,node2-1] = x
+            a[node_x-1,node_x-1] = x
+            a[node_x-1,node_y-1] = -x
+            a[node_y-1,node_x-1] = -x
+            a[node_y-1,node_y-1] = x
         J_x = LHS + a
         iteration_counter += 1
         
@@ -261,7 +296,7 @@ def Diode_system(LHS, RHS, node1, node2, Is, VT, maxi,maxj):
     # Here, either a solution is found, or too many iterations
     if error > eps:
         iteration_counter = -1
-    return v
+    return v, J_x, G_x
 
 
 def plu_solve(A, b):
@@ -272,11 +307,29 @@ def plu_solve(A, b):
     
     return back_substitution(U, y)
 
+# The variables for the sine wave voltage source
+t_d = 1e-3
+f = 1e3
+theta = 400
+V_a = 0.5
+V_o = 1
+
+# The variables for the pulse wave voltage source
+V1 = 0
+V2 = 1
+t1 = 0
+td = 1e-3
+tr = 0.5e-3
+tf = 0.2e-3
+tpw = 2e-3
+tper = 4e-3
+
 #Values of the variables
 Is = 3e-9 # A
 VT = 0.05 # V
+V1, temp = V_pulse(V1,V2,t1,td,tr,tf,tpw,tper)
 Vs = [
-    5
+    V1
 ]
 
 I = [
@@ -284,7 +337,7 @@ I = [
 ]
 
 R = [
-    1e3,4e3,3e3,2e3
+    2e3,3e3,4e3,2e3,1e3
 ]
 
 # total number of nodes and voltage sources that is contained to build the base matrix 
@@ -304,10 +357,11 @@ I_stamp = [
 T_RHS = mat_sum(I_stamp)
 
 R_stamp = [
-    R_assigner(1,3,cond(R[0]),Maxi,Maxj),
-    R_assigner(1,0,cond(R[1]),Maxi,Maxj),
+    R_assigner(1,2,cond(R[0]),Maxi,Maxj),
+    R_assigner(2,0,cond(R[1]),Maxi,Maxj),
     R_assigner(2,3,cond(R[2]),Maxi,Maxj),
-    R_assigner(2,0,cond(R[3]),Maxi,Maxj)
+    R_assigner(3,0,cond(R[3]),Maxi,Maxj),
+    R_assigner(3,0,cond(R[4]),Maxi,Maxj)
 ]
 
 # Adding the resistor values from the stamp to create the overall RHS matrix
@@ -315,11 +369,13 @@ T_LHS = mat_sum(R_stamp)
 
 # Adding the branch values from different stamp sources (eg. Voltage source, VCCS) which affects both LHS and RHS
 
+
 # Adding the voltage source
-RHS, G_mat = Vs_assigner(T_RHS, Vs[0], T_LHS, 0, 3)
+RHS, G_mat, column_val = Vs_assigner(T_RHS, Vs[0], T_LHS, 0, 1)
 
 print(G_mat)
 print(RHS)
+print(column_val)
 # Create the function matrices of the variables that are going to be used
 # new_LHS = np.dot(LHS, X(LHS))
 
@@ -332,29 +388,90 @@ print(RHS)
 # get the end time
 #et1 = time.time()
 
-st2 = tm.time()
+
+
+# st2 = tm.time()
 # The function that would calculate the G_matrix and RHS of the diode using Newton-Raphson 
-ans = Diode_system(G_mat, RHS, 1, 2, Is, VT, Maxi, Maxj)
+# temp, J_x, G_x = Diode_system(G_mat, RHS, 1, 2, Is, VT, Maxi, Maxj)
+# ans, J_x, G_x = Diode_system(J_x, G_x, 1, 3, Is, VT, Maxi, Maxj)
 # get the end time
-et2 = tm.time()
+# et2 = tm.time()
+
+simul = 1
+
+volt1 = np.ones((n,1))
+volt2 = np.ones((n,1))
+volt3 = np.ones((n,1))
+current = np.ones((n,1))
+# get the start time
+st = tm.time()
+match simul:
+    # 1 does the pulse wave voltage source
+    case 1:
+        for i in range (0, len(t)):
+            t1 = t1 + h
+            V_t, t1 = V_pulse(V1,V2,t1,td,tr,tf,tpw,tper)
+            v_value = np.array([[V_t]])
+            RHS[column_val-1,0] = v_value
+            x_solution = np.linalg.solve(G_mat,RHS)
+            # RHS = RHS + Vs_assigner(+)
+            
+            # Storing the final results at the given timestep
+            volt1[i] = x_solution[0]
+            volt2[i] = x_solution[1]
+            volt3[i] = x_solution[2]
+            current[i] = x_solution[3]
+        
+            # get the end time     
+        et = tm.time()   
+    
+        # Plotting the Graph
+        
+        plt.plot(t, volt1, color = 'red', label = '$nodal voltage 1$')
+        plt.plot(t, volt2, color = 'blue', label = '$nodal voltage 2$')
+        plt.plot(t, volt3, color = 'green', label = '$nodal voltage 3$')
+        plt.plot(t, current, color = 'yellow', label = '$current$')
+        plt.xlabel('Time ($s$)')
+        plt.ylabel('Variables ($v1$, $v2$, $v3$, current)')
+        plt.title('Variation of the Variables ($v1$, $v2$, $v3$, current) with Time\n')
+        plt.legend()
+        plt.grid(which='major')
+        plt.minorticks_on()
+        plt.grid(which='minor', alpha=0.2)
+    
+    # 2 does the sine wave voltage source
+    case 2:
+        for i in range (0, len(t)):
+            t1 = t1 + h
+            V_t = V_sine(V_o,V_a,t_d,theta,f,t1)
+            # Storing the final results at the given timestep
+            X1[i] = V_t
+        
+            # get the end time     
+        et = tm.time()   
+        # Plotting the Graph
+        plt.plot(t, X1, color = 'red', label = '$V_s$')
+        plt.xlabel('Time ($s$)')
+        plt.ylabel('Variables ($V_s$)')
+        plt.title('Variation of the Variables ($V_s$) with Time\n' 
+                    + 'Timestep, $h$ = {}'.format(h))
+        plt.legend()
+        plt.grid(which='major')
+        plt.minorticks_on()
+        plt.grid(which='minor', alpha=0.2)
+    case _:
+        print("Doing the OP analysis")
+        print(np.linalg.solve(G_mat,RHS))
+        et = tm.time() 
+
 
 #print(ans1)
-print(ans)
-print(ans[1]-ans[0])
+# print(ans)
+# print(ans[1]-ans[0])
 
 # get the execution time
 # elapsed_time1 = et1 - st1
-elapsed_time2 = et2 - st2
+elapsed_time2 = et - st
 print('Execution time:', elapsed_time2, 'seconds')
 
-# plt.plot(t, x[0], color = 'red', label = '$x_1$')
-# plt.plot(t, x[1], color = 'blue', label = '$x_2$')
-# plt.xlabel('Time ($s$)')
-# plt.ylabel('Variables ($x_1$, $x_2$)')
-# plt.title('Variation of the Variables ($x_1$, $x_2$) with Time\n')
-# plt.legend()
-# plt.grid(which='major')
-# plt.minorticks_on()
-# plt.grid(which='minor', alpha=0.2)
-
-# plt.show()
+plt.show()
--- circuit_test/Python/Circuit_codes/tests.py
+++ circuit_test/Python/Circuit_codes/tests.py
@@ -61,53 +61,114 @@ h = 0.001 	# time interval
 # Defining the Non-Linear Equations Function
 # x1 is the current variable, i, while x2 is the voltage variable, of the capacitor.
 
-
-# Defining the Time and Timestep for Transient Simulation
-t_start = 0
-t_end = 6e-3
 n = 5001
-h = (t_end - t_start) / (n-1)
-t = np.arange(t_start, t_end + h, h)
+
 
 # The variables for the sine wave voltage source
-X1 = np.ones((n,1))
 t_d = 1e-3
 f = 1e3
 theta = 400
 V_a = 0.5
 V_o = 1
+
+# The variables for the pulse wave voltage source
+V1 = 0
+V2 = 1
+td = 0
+tr = 0.5e-3
+tf = 0.2e-3
+tpw = 2e-3
+tper = 4e-3
+
+
+# Outer Time Loop
+
+# Doing the sine wave analysis
+def V_sine(V_o,V_a,t_d,theta,f,t1):
+    V_t1 = 0
+    if(t1 < t_d):
+        V_t1 = V_o
+    else:
+        V_t1 = V_o + V_a*np.exp(-theta*(t1-t_d))*np.sin(2*np.pi*f*(t1-t_d))
+    return V_t1
+
+# Doing the pulse wave analysis
+def V_pulse(V1,V2,t1,td,tr,tf,tpw,tper):
+    V_t1 = 0
+    if(t1 >= 0 and t1 < td):
+        V_t1 = V1
+    elif(t1 >= td and t1 < (td + tr)):
+        V_t1 = V1 + (V2-V1)*(t1-td)/tr
+    elif(t1 >= (td + tr) and t1 < (td + tr + tpw)):
+        V_t1 = V2
+    elif(t1 >= (td + tr + tpw) and t1 < (td + tr + tpw + tf)):
+        V_t1 = V2 + (V1 - V2)*(t1-(td+tr+tpw))/tf
+    elif(t1 >= (td + tr + tpw + tf) and t1 <= (td + tper)):
+        V_t1 = V1
+    else:
+        t1 = td
+    return V_t1, t1
+
+
+# Defining the Time and Timestep for Transient Simulation
+X1 = np.ones((n,1))
+t_start = 0
+t_end = 12e-3
 t1 = 0
+h = (t_end - t_start) / (n-1)
+t = np.arange(t_start, t_end + h, h)
+simul = 1
 
 # get the start time
 st = tm.time()
-# Outer Time Loop
-
-for i in range (0, len(t)):
-    t1 = t1 + h
-    if(t1 < t_d):
-        V_t = V_o
-    else:
-        V_t = V_o + V_a*np.exp(-theta*(t1-t_d))*np.sin(2*np.pi*f*(t1-t_d))
+match simul:
+    # 1 does the pulse wave voltage source
+    case 1:
+        for i in range (0, len(t)):
+            t1 = t1 + h
+            V_t, t1 = V_pulse(V1,V2,t1,td,tr,tf,tpw,tper)
+            # Storing the final results at the given timestep
+            X1[i] = V_t
         
-    # Storing the final results at the given timestep
-    X1[i] = V_t
+            # get the end time     
+        et = tm.time()   
+    
+        # Plotting the Graph
+        plt.plot(t, X1, color = 'red', label = '$V_s$')
+        plt.xlabel('Time ($s$)')
+        plt.ylabel('Variables ($V_s$)')
+        plt.title('Variation of the Variables ($V_s$) with Time\n' 
+                    + 'Timestep, $h$ = {}'.format(h))
+        plt.legend()
+        plt.grid(which='major')
+        plt.minorticks_on()
+        plt.grid(which='minor', alpha=0.2)
+    
+    # 2 does the sine wave voltage source
+    case 2:
+        for i in range (0, len(t)):
+            t1 = t1 + h
+            V_t = V_sine(V_o,V_a,t_d,theta,f,t1)
+            # Storing the final results at the given timestep
+            X1[i] = V_t
+        
+            # get the end time     
+        et = tm.time()   
+        # Plotting the Graph
+        plt.plot(t, X1, color = 'red', label = '$V_s$')
+        plt.xlabel('Time ($s$)')
+        plt.ylabel('Variables ($V_s$)')
+        plt.title('Variation of the Variables ($V_s$) with Time\n' 
+                    + 'Timestep, $h$ = {}'.format(h))
+        plt.legend()
+        plt.grid(which='major')
+        plt.minorticks_on()
+        plt.grid(which='minor', alpha=0.2)
+    case _:
+        print("Doing the OP analysis")
+        et = tm.time() 
+
 	
-# Plotting the Graph
-
-# get the end time
-et = tm.time()
-
-plt.plot(t, X1, color = 'red', label = '$x_1$')
-plt.xlabel('Time ($s$)')
-plt.ylabel('Variables ($x_1$, $x_2$)')
-plt.title('Variation of the Variables ($x_1$, $x_2$) with Time\n' 
-			+ 'Timestep, $h$ = {}'.format(h))
-plt.legend()
-plt.grid(which='major')
-plt.minorticks_on()
-plt.grid(which='minor', alpha=0.2)
-
-
 
 
 # get the execution time
