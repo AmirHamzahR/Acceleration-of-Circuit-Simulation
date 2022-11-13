47c7190089fa93e2869460f1c41d1242ad85b7ff
AmirHamzahR <s1945267@ed.ac.uk>
2022-11-13 19:19:26 +0000

    adding C++ codes for linear stamps

--- circuit_test/C++/Circuit_test/Transient_code.cpp
+++ circuit_test/C++/Circuit_test/Transient_code.cpp
@@ -1,40 +1,142 @@
 #include <iostream>
 #include "armadillo"
+#include <armadillo>
+#include <tuple>
 using namespace arma;
 using namespace std;
 
-int main(){
+// create resistor matrix stamp
+mat R_assigner(double node_x, double node_y, double R, double maxi, double maxj){
+    mat a = zeros(maxi,maxj);
+    if(node_x == 0){
+        a.row(node_y-1).col(node_y-1) = R;
+    }
+    else if(node_y == 0){
+        a.row(node_x-1).col(node_x-1) = R;
+    }
+    else{
+        a.row(node_x-1).col(node_x-1) = R;
+        a.row(node_x-1).col(node_y-1) = -R;
+        a.row(node_y-1).col(node_x-1) = -R;
+        a.row(node_y-1).col(node_y-1) = R;
+    }
+    return a;
+}
 
-   mat A = randu<mat>(5,5);
-   vec b = randu<vec>(5);
+// create a current matrix stamp
+mat Is_assigner(double node_x, double node_y, double I, int maxi, int maxj){
+    maxj = 1;
+    mat a = zeros(maxi,maxj);
+    if(node_x == 0){
+        a.row(node_y-1).col(0) = -I;
+    }
+    else if(node_y == 0){
+        a.row(node_x-1).col(0) = I;
+    }
+    else{
+        a.row(node_x-1).col(0) = I;
+        a.row(node_y-1).col(0) = -I;
+    }
+    return a;
+}
+// branch extender function
+mat branch_ext(mat M, int node_x, int node_y){
+    int M_cols = M.n_cols;
+    int M_rows = M.n_rows;
+    mat va = zeros(M_rows,1);
+    if(node_x == 0){
+        va.row(node_y-1).col(0) = 1;
+    }
+    else if(node_y == 0){
+        va.row(node_x-1).col(0) = 1;
+    }
+    else{
+        va.row(node_x-1).col(0) = -1;
+        va.row(node_y-1).col(0) = 1;
+    }
 
-    A.print("A =");
-    b.print("b =");
+    mat zero_ext = zeros(1,1);
+    mat ha = va.as_row();
+    mat haz = join_rows(ha,zero_ext);
 
-    // solve Ax = b
-    vec x = solve(A,b);
+    mat M1 = join_rows(M,va);
+    mat M2 = join_cols(M1,haz);
 
-    // print x
-    x.print("x =");
+    return M2;
+}
 
-    // find LU decomp of A, if needed, P is the permutation matrix
+// Voltage source stamp assigner
+pair<mat,mat> Vs_assigner(int node_x, int node_y, double V_value, mat LHS, mat RHS){
+
+    vec value(1);
+    value = V_value;
+    // Extending the branch at the LHS matrix
+    mat New_LHS = branch_ext(LHS, node_x, node_y);
+
+    // Assigning the value at RHS
+    mat New_RHS = join_cols(RHS, value);
+    return {New_LHS,New_RHS};
+}
+
+mat mat_sum(vector<mat> vector_of_matrices){
+    mat a = vector_of_matrices[0];
+    for (int i = 1; i < vector_of_matrices.size(); ++i) {
+        a = a + vector_of_matrices[i];
+    }
+    return a;
+}
+
+mat LU_solve(mat A, mat b){
     mat L, U, P;
     lu(L,U,P,A);
-
-    // print l
-    L.print("L = ");
-
-    // print U
-    U.print("U = ");
-    
-    //solve using PLU decomposition
     vec x1 = solve(trimatu(U), solve(trimatl(L), P*b));
 
-    // print PLU decomposition solution
-    x1.print("PLU solution:");
+    return x1;
+}
 
-    //Check that A = LU
-    //(A-(P.t()*L*U)).print("Test of LU decomposition:");
+double cond(double R){
+    return 1/R;
+}
+
+int main(int argc, const char ** argv){
+    // Total number of nodes
+    int T_nodes{5};
+    // Size of matrix
+    int Maxi{T_nodes};
+    int Maxj{Maxi};
+
+    // assigning the resistor stamps
+    vector<mat> Total_LHS = {
+        // Default state - R_assigner(0,0,0,Maxi,Maxj)
+        R_assigner(2,1,cond(2),Maxi,Maxj),
+        R_assigner(2,0,cond(3),Maxi,Maxj),
+        R_assigner(3,2,cond(4),Maxi,Maxj),
+        R_assigner(3,0,cond(2),Maxi,Maxj),
+        R_assigner(3,4,cond(1),Maxi,Maxj)
+    };
+
+    // assigning the current stamps
+    vector<mat> Total_RHS = {
+        // Default state - Is_assigner(0,0,0,Maxi,Maxj)
+        Is_assigner(4,0,3,Maxi,Maxj)
+    };
+
+    mat LHS = mat_sum(Total_LHS);
+    LHS.print("Resistor matrix =");
+    mat RHS = mat_sum(Total_RHS);
+    RHS.print("Current matrix =");
+
+    // Assigning a voltage source
+    pair<mat,mat> Matrix = Vs_assigner(1,0,3,LHS,RHS);
+    LHS = Matrix.first;
+    RHS = Matrix.second;
+    // // location of voltage value for transient simulation
+    int V_locate1 = RHS.n_cols;
+    // End of assignment a voltage sources
+
+    mat x = solve(LHS,RHS);
+
+    x.print("The OP analysis of the circuit is: ");
 
     return 0;
 }
--- circuit_test/C++/Circuit_test/test1.cpp
+++ circuit_test/C++/Circuit_test/test1.cpp
@@ -1,30 +1,105 @@
 #include <iostream>
 #include <armadillo>
+#include <tuple>
+#include <algorithm>
+#include <list>
+using namespace arma;
+using namespace std;
+
+mat branch_ext(mat M, int node_x, int node_y){
+    int M_cols = M.n_cols;
+    int M_rows = M.n_rows;
+    mat va = zeros(M_rows,1);
+    if(node_x == 0){
+        va.row(node_y-1).col(0) = 1;
+    }
+    else if(node_y == 0){
+        va.row(node_x-1).col(0) = 1;
+    }
+    else{
+        va.row(node_x-1).col(0) = -1;
+        va.row(node_y-1).col(0) = 1;
+    }
+
+    mat zero_ext = zeros(1,1);
+    mat ha = va.as_row();
+    mat haz = join_rows(ha,zero_ext);
+
+    mat M1 = join_rows(M,va);
+    mat M2 = join_cols(M1,haz);
+
+    return M2;
+}
+
+// mat mat_sum(mat list_of_matrices){
+//     mat a = list_of_matrices[0];
+//     for (int i = 0; i < list_of_matrices.n_elem; ++i) {
+//         a = a + list_of_matrices[i];
+//     }
+//     return a;
+// }
+
+pair<mat,mat> Vs_assigner(int node_x, int node_y, double V_value, mat LHS, mat RHS){
+
+    vec value(1);
+    value = V_value;
+    // Extending the branch at the LHS matrix
+    mat New_LHS = branch_ext(LHS, node_x, node_y);
+
+    // Assigning the value at RHS
+    mat New_RHS = join_cols(RHS, value);
+    return {New_LHS,New_RHS};
+}
+
+void print_v(std::vector <mat> const &a) {
+   std::cout << "The vector elements are : ";
+
+   for(int i=0; i < a.size(); i++)
+   std::cout << a.at(i) << ' ';
+   std::cout << "The size of the vector is " << a.size();
+}
 
 int main(int argc, const char ** argv){
-    //Initialize the random generator 
-    arma::arma_rng::set_seed_random();
+    mat LHS(5,5,fill::randu);
+    // mat RHS(5,1,fill::randu);
+    // LHS.print("The old LHS is:");
+    // RHS.print("The old RHS is:");
+    // // Assigning a voltage source
+    // pair<mat,mat> Matrix = Vs_assigner(3,0,3,LHS,RHS);
+    // LHS = Matrix.first;
+    // RHS = Matrix.second;
+    // // location of voltage value for transient simulation
+    // int V_locate1 = RHS.n_cols;
+    // // End of assignment
+    // LHS.print("The New LHS is:");
+    // RHS.print("The New RHS is:");
 
-    //Create a 4x4 random matrix and print it on the screeen
-    arma::Mat<double> A = arma::randu(4,4);
-    std::cout << "A:\n" << A << "\n";
-
-    //Multiply A with this transpose:
-    std::cout << "A * A.t() = \n";
-    std::cout << A * A.t() << "\n";
-    //Access/Modify rows and columns from the array:
-    A.row(0) = A.row(1) + A.row(3);
-    A.col(3).zeros();
-    std::cout << "add rows 1 and 3, store result in row 0, also fill 4th column with zeros:\n";
-    std::cout << "A:\n" << A << "\n";
-
-    //Create a new diagonal matrix using the main diagonal of A:
-    arma::Mat<double>B = arma::diagmat(A);
-    std::cout << "B:\n" << B << "\n";
-
-    //Save matrices A and B:
-    A.save("A_mat.txt", arma::arma_ascii);
-    B.save("B_mat.txt", arma::arma_ascii);
-
+    vector<mat> test = { LHS, LHS};
+    print_v(test);
+    
+    
     return 0;
 }
+    //Initialize the random generator 
+    // arma::arma_rng::set_seed_random();
+
+    // //Create a 4x4 random matrix and print it on the screeen
+    // arma::Mat<double> A = arma::randu(4,4);
+    // std::cout << "A:\n" << A << "\n";
+
+    // //Multiply A with this transpose:
+    // std::cout << "A * A.t() = \n";
+    // std::cout << A * A.t() << "\n";
+    // //Access/Modify rows and columns from the array:
+    // A.row(0) = A.row(1) + A.row(3);
+    // A.col(3).zeros();
+    // std::cout << "add rows 1 and 3, store result in row 0, also fill 4th column with zeros:\n";
+    // std::cout << "A:\n" << A << "\n";
+
+    // //Create a new diagonal matrix using the main diagonal of A:
+    // arma::Mat<double>B = arma::diagmat(A);
+    // std::cout << "B:\n" << B << "\n";
+
+    // //Save matrices A and B:
+    // A.save("A_mat.txt", arma::arma_ascii);
+    // B.save("B_mat.txt", arma::arma_ascii);
