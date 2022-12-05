2dbe1befc592fc5047fd2511850e4289be853f8e
AmirHamzahR <s1945267@ed.ac.uk>
2022-12-05 15:22:20 +0800

    Adding the changes for python code

--- circuit_test/Python/Circuit_codes/Transient_code.py
+++ circuit_test/Python/Circuit_codes/Transient_code.py
@@ -9,14 +9,17 @@ Expansions:
 
 import time as tm
 import numpy as np
+import scipy.sparse as sp
+import scipy.sparse.linalg as spl
 import matplotlib.pyplot as plt
+from numpy import inf
 
 from numpy import count_nonzero
 # The amount of iterations for the timestep, the higher the more accurate but uses more computing resources
 n = 5001 #5001 seems to be the sweet spot
 
 # Choosing the pulse wave for the transient simulation
-simul = 1
+simul = 4
 
 # Defining the Time and Timestep for Transient Simulation
 X1 = np.ones((n,1))
@@ -78,49 +81,76 @@ def mat_sum(list_of_mat):
     return a
 
 # A function that initiliaze the current sources value and input into the RHS matrices based on MNA stamps
-def Is_assigner(node_x,node_y,I,maxi,maxj):
-    maxj = 1
-    a = matrix(maxi,maxj)
+def Is_assigner(node_x,node_y,I,RHS):
+    size_RHS = np.shape(RHS)
+    a = matrix(size_RHS[0],size_RHS[1])
     if(node_x == 0):
         a[node_y-1][0] = -I
     elif(node_y == 0):
         a[node_x-1][0] = I
     else:
-        a[node_x-1][maxj-1] = I
-        a[node_y-1][maxj-1] = -I
-    return a
+        a[node_x-1][0] = I
+        a[node_y-1][0] = -I
+        
+    New_RHS = RHS + a
+    return New_RHS
 
 # A function that initiliaze the resistor values and input into the LHS matrices based on MNA stamps
-def R_assigner(node_x,node_y,R,maxi,maxj):
-    a = matrix(maxi,maxj)
-    if(node_x == 0):
-        a[node_y-1,node_y-1] = R
-    elif(node_y == 0):
-        a[node_x-1,node_x-1] = R
-    else:
-        a[node_x-1,node_x-1] = R
-        a[node_x-1,node_y-1] = -R
-        a[node_y-1,node_x-1] = -R
-        a[node_y-1,node_y-1] = R
-    return a
-
-def C_assigner(node_x,node_y,C,LHS,RHS,init,h):
-    # this if else statment uses trapezoidal formula
+def R_assigner(node_x,node_y,R,LHS):
     size_LHS = np.shape(LHS)
-    x = C/h
+    a = np.zeros((size_LHS[0],size_LHS[1]))
+    
     if(node_x == 0):
-        x1 = C*init[node_y-1]/h
+        a[node_y-1][node_y-1] = R
+        
     elif(node_y == 0):
-        x1 = C*init[node_x-1]/h
+        a[node_x-1][node_x-1] = R
     else:
-        x1 = C*(init[node_x-1]-init[node_y-1])/h
+        a[node_x-1][node_x-1] = R
+        a[node_x-1][node_y-1] = -R
+        a[node_y-1][node_x-1] = -R
+        a[node_y-1][node_y-1] = R
+        
+    New_LHS = LHS + a
+    return New_LHS
+
+# A function that initiliaze the resistor values and input into the LHS matrices based on MNA stamps
+def VCCS_assigner(node_x,node_y,node_cx,node_cy,R,LHS):
+    size_LHS = np.shape(LHS)
+    a = matrix(size_LHS[0],size_LHS[1])
+    a[node_x-1,node_cx-1] = R
+    a[node_x-1,node_cy-1] = -R
+    a[node_y-1,node_cx-1] = -R
+    a[node_y-1,node_cy-1] = R
+    
+    LHS = LHS + a
+    
+    return LHS
+
+def N_JFet_assigner(node_d,node_g,node_s,beta,Vto,LAMBDA,LHS,RHS,solution):
+    VCCS_assigner(node_d,node_s,node_d,node_s,R,LHS)
+    Diode_assigner(node_g,node_d,2.7e-9,0.05,4e-12,h,LHS,RHS,solution)
+    Diode_assigner(node_g,node_s,2.7e-9,0.05,4e-12,h,LHS,RHS,solution)
+    
+    return LHS, RHS
+
+def C_assigner(node_x,node_y,C,LHS,RHS,solution,h,mode):
+    x = C/h
+    if(mode == 0):
+        x1 = 0
+        x = 0
+    else:
+        if(node_x == 0):
+            x1 = C*solution[node_y-1]/h
+        elif(node_y == 0):
+            x1 = C*solution[node_x-1]/h
+        else:
+            x1 = C*(solution[node_x-1]-solution[node_y-1])/h
     
     # Matrix stamp for a capacitor on RHS
-    a = Is_assigner(node_x,node_y,x1,size_LHS[0],size_LHS[1])
+    New_RHS = Is_assigner(node_x,node_y,x1,RHS)
     # Matrix stamp for a capacitor on LHS
-    b = R_assigner(node_x,node_y,x,size_LHS[0],size_LHS[1]) 
-    New_LHS = LHS + b
-    New_RHS = RHS + a
+    New_LHS = R_assigner(node_x,node_y,x,LHS)
     
     return New_LHS, New_RHS
 
@@ -154,7 +184,10 @@ def Vs_assigner(V_value, node_x, node_y, LHS, RHS):
     # Extending the branch at the LHS matrix
     New_LHS = branch_ext(LHS,node_x,node_y)
     # Assigning the value at RHS
-    New_RHS = np.concatenate((RHS, Value), axis=0)
+    if(node_x == 0):
+        New_RHS = np.concatenate((RHS, -Value), axis=0)
+    else:
+        New_RHS = np.concatenate((RHS, Value), axis=0)
     size_RHS = np.shape(New_RHS)
     return New_LHS, New_RHS, size_RHS[0]
 
@@ -176,9 +209,82 @@ def Ind_assigner(L,node_x,node_y,LHS,RHS,h,init):
     
     return New_LHS, New_RHS
 
-def Diode_assigner(node_x,node_y,Is,VT,cd,h,LHS,RHS,solution):
-    size_LHS = np.shape(LHS)
+def fet_assigner(number, node_vd, node_vg, node_vs, node_vb, solution, LHS, RHS,  mode):
+    # the position of the drain, gate, source, and base voltages are hard-coded for the transistor model
+    # we are using a discrete MOSFET, so the vb is connected to the source terminal
+    add = 0
+    for i in range(number-1):
+        add +=14
+        i += 1
+        
+    # The settings for the large signal analysis model
+    # uses node_vd as starting reference node for the simulation
+    if(node_vd == 0):
+        LHS = R_assigner(0,1+4+add,0,LHS) # RD
+        node_vd = 1
+        vd = 0
+    else:
+        LHS = R_assigner(node_vd+add,node_vd+4+add,cond(1),LHS) # RD
+        vd = solution[node_vd-1+add,0]
+        
+    if(node_vg == 0):
+        vg = 0
+    else:
+        vg = solution[node_vg-1+add,0]
     
+    if(node_vs == 0):
+        vs = 0
+    else:
+        vs = solution[node_vs-1+add,0]
+        
+    if(node_vb == 0):
+        vb = 0
+    else:
+        vb = solution[node_vb-1+add,0]
+    # the other settings for fet model based on the large signal analysis
+    LHS = R_assigner(node_vg+add,node_vd+5+add,cond(1),LHS) # RG
+    LHS = R_assigner(node_vs+add,node_vd+7+add,cond(5e3),LHS) # RS
+    LHS = R_assigner(node_vb+add,node_vd+10+add,cond(1),LHS) # RB
+    LHS = R_assigner(node_vd+12+add,node_vd+13+add,cond(1e6),LHS) # RDS
+    LHS, RHS = Diode_assigner(node_vd+11+add,node_vd+12+add,2.7e-9,0.05,0,h,LHS,RHS,solution) # Diode BD
+    LHS, RHS = Diode_assigner(node_vd+9+add,node_vd+13+add,2.7e-9,0.05,0,h,LHS,RHS,solution) # Diode BS
+    LHS, RHS = C_assigner(node_vd+11+add,node_vd+4+add,0,LHS,RHS,solution,h,mode) # Capacitor BD
+    LHS, RHS = C_assigner(node_vd+4+add,node_vd+5+add,0,LHS,RHS,solution,h,mode) # Capacitor GD
+    LHS, RHS = C_assigner(node_vd+6+add,node_vd+7+add,0,LHS,RHS,solution,h,mode) # Capacitor GBO_1
+    LHS, RHS = C_assigner(node_vd+6+add,node_vb+8+add,0,LHS,RHS,solution,h,mode) # Capacitor GBO_2
+    LHS, RHS = C_assigner(node_vb+7+add,node_vb+8+add,0,LHS,RHS,solution,h,mode) # Capacitor BSO
+        
+    W = 400e-6
+    L = 10e-6
+    kp = 20e-6
+    mCox = kp
+    phi = 0.6
+    vt0 = 2
+    LAMBDA = 0
+    gamma = 0
+    K = 1/2*(mCox)*(W/L)
+    vsb = vs - vb
+    vt = vt0 + gamma*((np.sqrt(phi+vsb)-np.sqrt(phi))) # already taking into account the body effect of MOSFETs
+    vgs = vg - vs
+    vds = vd - vs
+    if (vgs>vt) and (vds <= vgs-vt):
+        id = K*(vgs - vt)*vds-np.power(vds,2)*(1+LAMBDA*vds)
+    elif (vgs>vt) and (vds >= vgs-vt):
+        id = K * np.power((vgs - vt),2) * (1+LAMBDA*vds)
+    else:
+        id=0
+        
+    print(vgs,vt,vds,id)
+    # the position of the drain current is hard-coded for the transistor model
+    if(node_vd == 0):
+        New_RHS = Is_assigner(1+12+add,1+13+add,id,RHS)
+    else:
+        New_RHS = Is_assigner(node_vd + 12+add, node_vd + 13+add,id,RHS)
+    New_LHS = LHS
+    print(New_RHS,"\n",New_LHS)
+    return New_LHS,New_RHS
+
+def Diode_assigner(node_x,node_y,Is,VT,cd,h,LHS,RHS,solution):
     if(node_x == 0):
         x = (Is/VT)*(np.exp((-solution[node_y-1])/VT)) + cd/h
         x1 = (x*(-solution[node_y-1])-Is*(np.exp((-solution[node_y-1])/VT)-1))
@@ -190,12 +296,9 @@ def Diode_assigner(node_x,node_y,Is,VT,cd,h,LHS,RHS,solution):
         x1 = (x*(solution[node_x-1]-solution[node_y-1])-Is*(np.exp((solution[node_x-1]-solution[node_y-1])/VT)-1))
     
     # Matrix stamp for a diode on RHS
-    a = Is_assigner(node_x,node_y,x1,size_LHS[0],size_LHS[1])
+    New_RHS = Is_assigner(node_x,node_y,x1,RHS)
     # Matrix stamp for a diode on LHS
-    b = R_assigner(node_x,node_y,x,size_LHS[0],size_LHS[1])
-    
-    New_LHS = LHS + b
-    New_RHS = RHS + a
+    New_LHS = R_assigner(node_x,node_y,x,LHS)
     
     return New_LHS, New_RHS
 
@@ -213,6 +316,7 @@ def NewtonRaphson_system(RHS,LHS,h,init):
     solution = init
     
     # Label the non-linear and dynamic components here
+    LHS, RHS = fet_assigner(1, 1, 0, 3, 3, LHS, RHS)
     LHS, RHS = C_assigner(3,0,C[0],LHS,RHS,solution,h)
     LHS, RHS = C_assigner(4,0,C[1],LHS,RHS,solution,h)
     LHS, RHS = Diode_assigner(2,3,2.7e-9,0.05,4e-12,h,LHS,RHS,solution)
@@ -226,12 +330,12 @@ def NewtonRaphson_system(RHS,LHS,h,init):
         solution -= delta
         
         iteration_counter += 1
-        print(iteration_counter)
+        # print(iteration_counter)
         
     return solution
 
 # Function that linearize the DC components using NR algorithm during operating point, t = 0
-def OPanalysis_system(RHS,LHS):
+def OPanalysis_system(LHS,RHS):
     """
     Solve nonlinear system F=0 by Newton's method.
     J is the Jacobian of F. Both F and J must be functions of x.
@@ -244,79 +348,58 @@ def OPanalysis_system(RHS,LHS):
     size = np.shape(LHS)
     solution = np.zeros((size[0],1))
     
-    # Label the non-linear components here
-    LHS, RHS = Diode_assigner(2,3,2.7e-9,0.05,0,h,LHS,RHS,solution)
-    # LHS, RHS = Ind_assigner(0,2,0,LHS,RHS,h,solution)
-    
-    size = np.shape(LHS)
-    solution = np.zeros((size[0],1))
-    
     # iteration counter set to 50 as followed by the SPICE OPUS textbook
     while error > eps and iteration_counter < 50:
-        
+        LHS, RHS = fet_assigner(1, 1, 0, 3, 3, solution, LHS, RHS, 0)
+        np.savetxt('mat_LHS.csv',LHS, fmt = '%f', delimiter=",")
+        print(iteration_counter)
+        # if(sp.issparse(LHS)):
+        # delta = spl.spsolve(LHS, (np.matmul(LHS,solution) - RHS))
+        # else:
         delta = np.linalg.solve(LHS, np.matmul(LHS,solution) - RHS)
+        print(delta)
         error = np.max(np.abs(delta))
         solution -= delta
         
         iteration_counter += 1
+        
     # saves the LHS, RHS, and solution matrices in CSV files
-    np.savetxt('mat_Solution.csv',solution, fmt = '%f', delimiter=",") 
+    np.savetxt('mat_Solution.csv',solution, fmt = '%f', delimiter=",")
     np.savetxt('mat_LHS.csv',LHS, fmt = '%f', delimiter=",")
-    np.savetxt('mat_RHS.csv',RHS, fmt = '%f', delimiter=",")  
+    np.savetxt('mat_RHS.csv',RHS, fmt = '%f', delimiter=",")
     # print(1.0 - count_nonzero(LHS) / LHS.size) # just to count the non-zeros and see how sparse it is
     
-    return LHS, RHS, solution 
+    return LHS, RHS, solution
 
-# total number of nodes and voltage sources that is contained to build the base matrix 
-# that contains only zeros which will then be occupied with values from the components
-T_nodes = 4
+''' 
+The total number of nodes and voltage sources that is contained to build the base matrix
+that contains only zeros which will then be occupied with values from the components.
+    - a normal 2 port component will add 1/2 nodes depending on how it is connected
+    - each FET transistor adds 14 nodes
+'''
+T_nodes = 14
 
 # Size of matrix
 Maxi = T_nodes
 Maxj = Maxi
 
+
+LHS = np.zeros((Maxi,Maxj)) # LHS matrix
+RHS = np.zeros((Maxi,1)) # RHS matrix
+
 # Values of the variables
 # Initial voltage before turning on:
-V1 = 2
-Vs = [
-    V1
-]
-
-I = [
-    0
-]
-
-C = [
-    0.4e-6,0.8e-6
-]
-
-R = [
-    3e3,1e3
-]
-
-# Resistor values in a similar format of SPICE simulators's netlist
-I_stamp = [
-    # default state (for RHS) - Is_assigner(0,0,I[0],Maxi,Maxj)
-    Is_assigner(0,0,0,Maxi,Maxj)
-]
-
-# Adding the current values from the stamp to create the overall RHS matrix 
-RHS = mat_sum(I_stamp)
-R_stamp = [
-    # default state (for LHS) - R_assigner(0,0,0,Maxi,Maxj)
-    R_assigner(2,1,cond(R[0]),Maxi,Maxj),
-    R_assigner(4,3,cond(R[1]),Maxi,Maxj)
-]
-
-# Adding the resistor values from the stamp to create the overall RHS matrix
-LHS = mat_sum(R_stamp)
+# V1 = 2
 
 # Adding the branch values from different stamp sources (eg. Voltage source, VCCS) which affects both LHS and RHS
-LHS, RHS, Vs_locate = Vs_assigner(Vs[0], 1, 0, LHS, RHS)
+LHS, RHS, temp = Vs_assigner(5, 1, 0, LHS, RHS)
+LHS, RHS, temp = Vs_assigner(5, 0, 3, LHS, RHS)
+# LHS, RHS, Vs_locate = Vs_assigner(Vs[0], 1, 0, LHS, RHS)
 
 # OP analysis for the initial conditions for the transient simulation
-LHS, RHS, init = OPanalysis_system(RHS,LHS)
-
+LHS, RHS, init = OPanalysis_system(LHS,RHS)
+print("LHS:",LHS)
+print("RHS:",RHS)
 print("OP analysis results:\n",init)
 
 volt1 = np.ones((n,1))
@@ -324,7 +407,8 @@ volt2 = np.ones((n,1))
 volt3 = np.ones((n,1))
 volt4 = np.ones((n,1))
 current = np.ones((n,1))
-volt5 = np.ones((n,1))
+RHS1 = np.ones((n,1))
+RHS2 = np.ones((n,1))
 
 # get the start time
 st = tm.time()
@@ -349,7 +433,6 @@ match simul:
             volt3[i] = x_solution[2]
             volt4[i] = x_solution[3]
             current[i] = x_solution[4]
-            volt5[i] = x_solution[4]
             
         et = tm.time()
         # Plotting the Graph
@@ -359,7 +442,7 @@ match simul:
         plt.plot(t, volt3, color = 'green', label = '$nodal voltage 3$')
         plt.plot(t, volt4, color = 'black', label = '$nodal voltage 4$')
         plt.plot(t, current, color = 'yellow', label = '$current$')
-        plt.plot(t, volt5, color = 'yellow', label = '$nodal voltage 5$')
+        plt.plot(t, RHS2-RHS1, color = 'pink', label = '$resistor current$')
         plt.xlabel('Time ($s$)')
         plt.ylabel('Variables ($v1$, $v2$, $v3$, current)')
         plt.title('Variation of the Variables ($v1$, $v2$, $v3$, current) with Time\n')
